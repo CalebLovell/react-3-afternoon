bd5dfad2f89214307dcbf09b7c8f21d825749ba5
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-14 16:22:09 -0600

    finish step 1-6

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Post from './Post/Post'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+
+let baseUrl = 'https://practiceapi.devmountain.com/api'
 
 class App extends Component {
   constructor() {
@@ -19,20 +23,36 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+      .get(`${baseUrl}/posts`)
+      .then( results => {
+        this.setState({posts: results.data});
+      });
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios
+      .put(`${baseUrl}/posts?id=${ id }`, { text} )
+      .then( results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`${baseUrl}/posts?id=${ id }`)
+      .then( results => {
+        this.setState({posts: results.data});
+      });
   }
 
-  createPost() {
-
-  }
+  createPost(text) {
+    axios
+      .post(`${baseUrl}/posts`, { text })
+      .then( results => {
+        this.setState({posts: results.data});
+      });
+    }
 
   render() {
     const { posts } = this.state;
@@ -43,8 +63,19 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+
+          {
+            posts.map ( post => (
+              <Post key={post.id} 
+                    text={post.text}
+                    date={post.date}
+                    id={ post.id }
+                    updatePostFn={this.updatePost}
+                    deletePostFn={this.deletePost} />
+            ))
+          }
+
         </section>
       </div>
     );
